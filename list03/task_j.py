03031aaf98ec67205260ca9859a5266de43465f2
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-02-25 15:19:20 -0300

    :construction:

--- list03/task_j.py
+++ list03/task_j.py
@@ -1,9 +1,11 @@
+# recebe as coordenadas do jogador
 inicio = input()
 inicio = inicio.split(" ")
 coordenada = []
 for i in inicio:
     coordenada.append(int(i))
 
+# recebe as coordenadas do squad e indica a direcao
 inicioSquad = input()
 inicioSquad = inicioSquad.split(" ")
 coordenadaSquad = []
@@ -17,6 +19,8 @@ else:
 sucesso = True
 for m in range(0, 3):
     nomeLocal = input()
+
+    # caso seja a ultima rodada, recebe as coordenadas dos policiais
     if m == 2:
         inicioPoliciais = input()
         inicioPoliciais = inicioPoliciais.split(" ")
@@ -24,9 +28,9 @@ for m in range(0, 3):
         for i in inicioPoliciais:
             coordenadaPoliciais.append(int(i))
 
+
     tamanhoMatrizes = int(input())
     matrizes = []
-
 
     # recebe a primeira matriz
     primeiraMatriz = []
@@ -54,12 +58,12 @@ for m in range(0, 3):
             somaLinha.append(int(matrizes[0][i][j]) + int(matrizes[1][i][j]))
         somaMatrizes.append(somaLinha)
 
-
     # soma todos elementos da matriz resultante
     resultadoSoma = 0
     for i in range(tamanhoMatrizes):
         for j in range(tamanhoMatrizes):
             resultadoSoma += somaMatrizes[i][j]
+
 
     # multiplica as matrizes
     produtoMatrizes = []
@@ -77,29 +81,39 @@ for m in range(0, 3):
     for i in range(tamanhoMatrizes):
         resultadoProduto += produtoMatrizes[i][i]
 
+    # cria as coordenadas do objetivo com base nas operações das matrizes
     coordenadasObjetivo = [resultadoSoma, resultadoProduto]
+
+    # calcula a qtd de Passos para o objetivo
     qtdPassos = abs(coordenadasObjetivo[0]-coordenada[0]) + abs(coordenadasObjetivo[1]-coordenada[1])
+
+    # calcula as distancias para o objetivo
     distanciaVertical = coordenadasObjetivo[1]-coordenada[1]
+    distanciaHorizontal = coordenadasObjetivo[0] - coordenada[0]
 
     if m == 2:
         distanciaVerticalPoliciais = coordenadasObjetivo[1] - coordenadaPoliciais[1]
         distanciaHorizontalPoliciais = coordenadasObjetivo[0] - coordenadaPoliciais[0]
 
-    # movimentacao
+    # movimentacao dos protagonistas na vertical
     while coordenada[1] != coordenadasObjetivo[1]:
+        # if m == 2:
+        #     print(coordenada, coordenadaPoliciais)
         if not sucesso:
             break
+
+        # verifica se tem que andar para cima ou para baixo
         if distanciaVertical > 0:
             coordenada[1] = coordenada[1]+1
         else:
             coordenada[1] = coordenada[1]-1
 
+        # faz a movimentação do Squad
         if movimentacaoSquad == "direita":
             if coordenadaSquad[0] == 7:
                 movimentacaoSquad = "esquerda"
             else:
                 coordenadaSquad[0] = coordenadaSquad[0]+1
-
         if movimentacaoSquad == "esquerda":
             if coordenadaSquad[0] == 0:
                 movimentacaoSquad = "direita"
@@ -107,6 +121,7 @@ for m in range(0, 3):
             else:
                 coordenadaSquad[0] = coordenadaSquad[0]-1
 
+        # faz a movimentacao dos policiais
         if m == 2:
             if coordenadaPoliciais[0] != coordenadasObjetivo[0]:
                 if distanciaHorizontalPoliciais > 0:
@@ -119,11 +134,19 @@ for m in range(0, 3):
                 else:
                     coordenadaPoliciais[1] = coordenadaPoliciais[1] - 1
 
+        # caso os policiais chegem na coordenada antes dos protagonistas eles ganham
         if m == 2:
-            if coordenada[0] == coordenadaPoliciais[0] and coordenada[1] == coordenadaPoliciais[1]:
+            if coordenadaPoliciais == coordenadasObjetivo and coordenada != coordenadasObjetivo:
                 sucesso = False
-                break
 
+        if m == 2:
+            if coordenada != coordenadasObjetivo:
+                # caso os protagonistas se encontrem com o policiais e nao for no objetivo, eles perdem
+                if coordenada[0] == coordenadaPoliciais[0] and coordenada[1] == coordenadaPoliciais[1]:
+                    sucesso = False
+                    break
+
+        # caso os protagonistas se encontrem com o Squad, eles perdem
         if coordenada[0] == coordenadaSquad[0] and coordenada[1] == coordenadaSquad[1]:
             print("Parado! Está cercado pelo Esquadrão De Segurança para Evitar o Paradoxo do Tempo. O que quer que você diga pode ou já foi usado contra você no Tribunal do Futuro.")
             print("Talvez não foi uma boa ideia ajudar o Ford…")
@@ -133,22 +156,25 @@ for m in range(0, 3):
     if not sucesso:
         break
 
-    # movimentacao
-    distanciaHorizontal = coordenadasObjetivo[0] - coordenada[0]
+    # movimentacao dos protagonistas na vertical
     while coordenada[0] != coordenadasObjetivo[0]:
+        # if m == 2:
+        #     print(coordenada, coordenadaPoliciais)
         if not sucesso:
             break
+
+        # verifica se tem que andar para direita ou esquerda
         if distanciaHorizontal > 0:
             coordenada[0] = coordenada[0] + 1
         else:
             coordenada[0] = coordenada[0] - 1
 
+        # faz a movimentação do Squad
         if movimentacaoSquad == "direita":
             if coordenadaSquad[0] == 7:
                 movimentacaoSquad = "esquerda"
             else:
                 coordenadaSquad[0] = coordenadaSquad[0] + 1
-
         if movimentacaoSquad == "esquerda":
             if coordenadaSquad[0] == 0:
                 movimentacaoSquad = "direita"
@@ -156,6 +182,7 @@ for m in range(0, 3):
             else:
                 coordenadaSquad[0] = coordenadaSquad[0] - 1
 
+        # faz a movimentacao dos policiais
         if m == 2:
             if coordenadaPoliciais[0] != coordenadasObjetivo[0]:
                 if distanciaHorizontalPoliciais > 0:
@@ -168,11 +195,19 @@ for m in range(0, 3):
                 else:
                     coordenadaPoliciais[1] = coordenadaPoliciais[1] - 1
 
+        # caso os policiais chegem na coordenada antes dos protagonistas eles ganham
         if m == 2:
-            if coordenada[0] == coordenadaPoliciais[0] and coordenada[1] == coordenadaPoliciais[1]:
+            if coordenadaPoliciais == coordenadasObjetivo and coordenada != coordenadasObjetivo:
                 sucesso = False
-                break
 
+        if m == 2:
+            if coordenada != coordenadasObjetivo:
+                # caso os protagonistas se encontrem com o policiais e nao for no , eles perdem
+                if coordenada[0] == coordenadaPoliciais[0] and coordenada[1] == coordenadaPoliciais[1]:
+                    sucesso = False
+                    break
+
+        # caso os protagonistas se encontrem com o Squad, eles perdem
         if coordenada[0] == coordenadaSquad[0] and coordenada[1] == coordenadaSquad[1]:
             print("Parado! Está cercado pelo Esquadrão De Segurança para Evitar o Paradoxo do Tempo. O que quer que você diga pode ou já foi usado contra você no Tribunal do Futuro.")
             print("Talvez não foi uma boa ideia ajudar o Ford…")
