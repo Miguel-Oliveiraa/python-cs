99f759386bb6fad2dcbdce273e94d08d11713d84
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-03-03 23:32:33 -0300

    :construction:

--- list01/task_b.py
+++ list01/task_b.py
@@ -1,24 +1,24 @@
 selecaoUm = str(input())
 selecaoDois = str(input())
 valorApostado = int(input())
-probabilidade = float(input())
+probabilidadebilidade = float(input())
 resultado = str(input())
 
-retorno = int(valorApostado * (1+(0.5 - probabilidade)**2 * 100))
+retorno = int(valorApostado * (1+(0.5 - probabilidadebilidade)**2 * 100))
 
-if probabilidade >= 0.5:
+if probabilidadebilidade >= 0.5:
     print("Pedro, você está proibido de apostar nos favoritos, só em zebra, lembra?")
 else:
   if resultado == "Ganhou":
-      if probabilidade > 0.4:
+      if probabilidadebilidade > 0.4:
         print(f"Não foi um palpite tão arriscado, todos sabiam que {selecaoUm} não estava muito atrás de {selecaoDois}")
-      elif probabilidade > 0.3:
+      elif probabilidadebilidade > 0.3:
         print(f"Eu pensava que {selecaoDois} iria ganhar, mas nunca duvidei que a {selecaoUm} pudesse ganhar essa partida")
-      elif probabilidade > 0.2:
+      elif probabilidadebilidade > 0.2:
         print(f"Meu Deus! que jogo foi esse?? {selecaoUm} surpreendeu a todos nós…")
-      elif probabilidade > 0.1:
+      elif probabilidadebilidade > 0.1:
         print(f"Essa é a copa das zebras?? O impossível aconteceu hoje nessa partida, como que {selecaoUm} ganhou de {selecaoDois}, alguém me explica?")
-      elif probabilidade <= 0.1:
+      elif probabilidadebilidade <= 0.1:
         print(f"PEDROOOOO, você tá rico!! ninguém, absolutamente ninguém apostou na {selecaoUm}, somente você!")
       print(f"Parabéns, você apostou R${valorApostado} e recebeu R${retorno} nessa aposta")
   else:
---
+++ list04/task_b.py
@@ -0,0 +1,46 @@
+def probabilidadebilidade(nome):
+  global lista
+  global qtdMorte
+  global qtdVivo
+  if (len(nome) > 7):
+    print(f'Er {nome[0]}{nome[1]}.. errr... nao consigo lembrar, melhor deixar para la')
+  else:
+    if (nome == 'Makima'):
+      print(f"Woof Woof")
+      probabilidade = int(input())
+      print(f"Beleza {nome}!! Essa deve ser molezinha!")
+      qtdVivo += 1
+    else:
+      probabilidade = int(input())
+      if (probabilidade > 50):
+        print(
+          f"{nome}, mais uma que so quer o coraçao do chainsaw man, quando que alguem vai querer o meu coraçao!?!?")
+        qtdMorte += 1
+        if (lista == ''):
+          lista += f'nome: {nome} - chances de morrer: {probabilidade}%'
+        else:
+          lista += f'\nnome: {nome} - chances de morrer: {probabilidade}%'
+      elif (probabilidade <= 50):
+        print(f"Beleza {nome}!! Essa deve ser molezinha!")
+        qtdVivo += 1
+        if (lista == ''):
+          lista += f'nome: {nome} - chances de morrer: {probabilidade}%'
+        else:
+          lista += f'\nnome: {nome} - chances de morrer: {probabilidade}%'
+
+
+qtdMorte = 0
+qtdVivo = 0
+lista = ''
+entrada = input()
+while (entrada != 'cabo'):
+  probabilidadebilidade(entrada)
+  entrada = input()
+
+if (qtdVivo > qtdMorte):
+  print("Epa ai sim! E hoje pochita!!")
+else:
+  print("Desculpa pochita acho que nao vai ser hoje que voce vai poder ver meus sonhos")
+
+if (qtdMorte == 0):
+  print(lista)
