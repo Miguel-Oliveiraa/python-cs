4006b9a9473c066936ffa5ebe7da115d30412074
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-03-04 16:36:59 -0300

    :construction:

--- list04/task_f.py
+++ list04/task_f.py
@@ -4,18 +4,21 @@ def transformaAscii(nome):
     c += ord(i)
   return c
 
+
+# procura a posicao live e aloca prisioneiro
 def alocarPrisioneiro(celas, posicao, nomePrisioneiro, quantidade_celas):
-  if celas[posicao] == None:
+  if celas[posicao] == " ":
     celas[posicao] = nomePrisioneiro
   else:
-    while celas[posicao] != None:
+    while celas[posicao] != " ":
       posicao = (posicao + 1) % quantidade_celas
     celas[posicao] = nomePrisioneiro
 
 
+# recebe os inputs iniciais
 mensagem = input().split(" ")
 quantidade_celas, quantidade_ordens = int(mensagem[0]), int(mensagem[1])
-celas = [None for _ in range(quantidade_celas)]
+celas = [" "] * quantidade_celas
 qtdPrisioneiros = 0
 
 for i in range(0, quantidade_ordens):
@@ -29,23 +32,27 @@ for i in range(0, quantidade_ordens):
     if qtdPrisioneiros == quantidade_celas:
       print("CHEIO")
     else:
+      # aloca o prisioneiro e incrementa o contador
       alocarPrisioneiro(celas, posicao, nomePrisioneiro, quantidade_celas)
       qtdPrisioneiros += 1
   elif comando == "REMOVER":
     if celas[posicao] == nomePrisioneiro:
-      celas[posicao] = None
+      # remove o prisioneiro e decrementa o contador
+      celas[posicao] = " "
       qtdPrisioneiros -= 1
     else:
       if nomePrisioneiro not in celas:
         print("NAO EXISTE")
       else:
+        # remove o prisioneiro e decrementa o contador
         posicao = celas.index(nomePrisioneiro)
-        celas[posicao] = None
+        celas[posicao] = " "
         qtdPrisioneiros -= 1
 
+
+# imprime os prisioneiros em ordem
 ordemPrisioneiros = []
 for i in range(len(celas)):
-  if type(celas[i]) == str:
+  if celas[i] != " ":
     ordemPrisioneiros.append(celas[i])
-
 print(*ordemPrisioneiros, sep=" ")
