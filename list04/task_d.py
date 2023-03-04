5e0ae8f61b3b681c919b5230b94478feb594cb3b
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-03-04 16:27:48 -0300

    :construction:

--- list04/task_d.py
+++ list04/task_d.py
@@ -1,4 +1,8 @@
-def verificaBonus(vidaDenji, vidaDemonios, ataqueDenji, ataqueDemonios, qtdDemonios, mensagem):
+def calcularDemonio(demonios):
+  posicao = len(demonios) - 1
+  return posicao
+
+def verificaBonus(vidaDenji, ataqueDenji, vidaDemonios, ataqueDemonios, qtdDemonios, mensagem):
   if mensagem == "Denji ganhou um beijo de Makima":
     vidaDenji += 50
   elif mensagem == "Pochita chegou para a batalha!":
@@ -13,8 +17,31 @@ def verificaBonus(vidaDenji, vidaDemonios, ataqueDenji, ataqueDemonios, qtdDemon
     vidaDemonios *= 2
     ataqueDemonios *= 2
     qtdDemonios *= 2
+  return vidaDenji, ataqueDenji, vidaDemonios, ataqueDemonios
+
+def batalha(vidaDenji, ataqueDenji, demonios, posicaoDemonio, qtdDemonios):
+  if (demonios[posicaoDemonio][0] - ataqueDenji) > 0:
+    if qtdDemonios != 1:
+      print("DENJI")
+      print(f"Vida: {vidaDenji}")
+      print(f"Ataque atual: {ataqueDenji}")
+      print("DEMÔNIO")
+      print(f"Vida: {demonios[posicaoDemonio-1][0]}")
+      print(f"Ataque atual: {demonios[posicaoDemonio-1][1]}")
+  else:
+    print("DENJI")
+    print(f"Vida: {vidaDenji}")
+    print(f"Ataque atual: {ataqueDenji}")
+    print("DEMÔNIO")
+    print(f"Vida: {demonios[posicaoDemonio][0]}")
+    print(f"Ataque atual: {demonios[posicaoDemonio][1]}")
+  demonios[posicaoDemonio][0] -= ataqueDenji
+  vidaDenji -= demonios[posicaoDemonio][1]
+  return vidaDenji, demonios[posicaoDemonio][0]
 
 qtdDemonios = int(input())
+demoniosMortos = 0
+demonios = []
 
 if qtdDemonios>0:
   vidaDenji = int(input())
@@ -22,10 +49,35 @@ if qtdDemonios>0:
   ataqueDenji = int(input())
   ataqueDemonios = int(input())
 
+  for i in range(qtdDemonios):
+    demonios.append([vidaDemonios, ataqueDemonios])
 
-  acaoDenji = input()
-  verificaBonus(vidaDenji, vidaDemonios, ataqueDenji, ataqueDemonios, qtdDemonios, acaoDenji)
-  acaoDemonio = input()
-  verificaBonus(vidaDenji, vidaDemonios, ataqueDenji, ataqueDemonios, qtdDemonios, acaoDemonio)
+  while len(demonios)>0 and vidaDenji>0:
+    posicaoDemonio = calcularDemonio(demonios)
+
+    # batalha
+    vidaDenji, demonios[posicaoDemonio][0] = batalha(vidaDenji, ataqueDenji, demonios, posicaoDemonio, len(demonios))
+    # verifica se o demonio morreu
+    if demonios[posicaoDemonio][0] <= 0:
+      if len(demonios) != 1:
+        print("Matei um!!!")
+      demonios.pop(posicaoDemonio)
+      posicaoDemonio -= 1
+      demoniosMortos += 1
+
+    # verificacao de bonus
+    acaoDenji = input()
+    vidaDenji, ataqueDenji, demonios[posicaoDemonio][0], demonios[posicaoDemonio][1] = verificaBonus(vidaDenji, ataqueDenji, demonios[posicaoDemonio][0], demonios[posicaoDemonio][1], qtdDemonios, acaoDenji)
+    acaoDemonio = input()
+    vidaDenji, ataqueDenji, demonios[posicaoDemonio][0], demonios[posicaoDemonio][1] = verificaBonus(vidaDenji, ataqueDenji, demonios[posicaoDemonio][0], demonios[posicaoDemonio][1], qtdDemonios, acaoDemonio)
+
 else:
   print("Uhuuul um dia só para relaxar!")
+
+if vidaDenji<=0:
+  print("Infelizmente Denji foi de comes e bebes :(")
+elif len(demonios)==0:
+  if demoniosMortos == qtdDemonios:
+    print("Ufa, agora posso descansar em paz!")
+  else:
+    print("Foi mais do que eu esperava mas finalmente terminei…")
