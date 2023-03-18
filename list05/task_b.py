36bd58c6635e410130851545299718c2f744426b
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-03-17 23:17:31 -0300

    :construction:

---
+++ list05/task_b.py
@@ -0,0 +1,9 @@
+codigo = input()
+
+def inverterString(codigo, contador):
+    if contador == 0:
+      return codigo[contador]
+    else:
+      return codigo[contador] + inverterString(codigo, contador-1)
+
+print(inverterString(codigo, len(codigo)-1))
