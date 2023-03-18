80237173ce6d4c43a4c0a186df6e0fa535d8c166
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-03-17 23:15:04 -0300

    :construction:

---
+++ list05/task_a.py
@@ -0,0 +1,11 @@
+numeros = input().split(" ")
+alvo = int(input())
+razao = int(numeros[1]) - int(numeros[0])
+
+def calcularNumero(numero, alvo, contador, razao):
+  if contador == alvo:
+    return numero
+  else:
+    return calcularNumero(numero + razao, alvo, contador+1, razao)
+
+print(f"Na progressão aritmética cujos três primeiros números são {numeros[0]}, {numeros[1]} e {numeros[2]}, o {alvo}º elemento é {calcularNumero(int(numeros[0]), alvo, 1, razao)} e a razão da progressão é {razao}.")
