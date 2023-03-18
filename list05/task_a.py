1504d470b059c5546b86eaacf966af1687bd1a4c
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-03-17 23:15:59 -0300

    :construction:

--- list05/task_a.py
+++ list05/task_a.py
@@ -8,4 +8,5 @@ def calcularNumero(numero, alvo, contador, razao):
   else:
     return calcularNumero(numero + razao, alvo, contador+1, razao)
 
-print(f"Na progressão aritmética cujos três primeiros números são {numeros[0]}, {numeros[1]} e {numeros[2]}, o {alvo}º elemento é {calcularNumero(int(numeros[0]), alvo, 1, razao)} e a razão da progressão é {razao}.")
+print(f"Na progressão aritmética cujos três primeiros números são {numeros[0]}, {numeros[1]} e {numeros[2]}, o {alvo}º "
+      f"elemento é {calcularNumero(int(numeros[0]), alvo, 1, razao)} e a razão da progressão é {razao}.")
