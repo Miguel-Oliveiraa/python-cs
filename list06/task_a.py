b3567ae1e96a2b86cbe67fb0453c3f9aa26a68b5
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-04-01 11:24:37 -0300

    :construction:

--- list06/task_a.py
+++ list06/task_a.py
@@ -1,20 +1,28 @@
 def main():
   numeroAnimais = int(input())
   dicionario = {"nomeAnimal": [], "especie": [], "dataNascimento": []}
+
+  # recebendo dados dos animais
   for i in range(numeroAnimais):
     linha = input().split(" ")
     dicionario["nomeAnimal"].append(linha[0]), dicionario["especie"].append(linha[1]), dicionario[
       "dataNascimento"].append(linha[2])
+
   mesSelecionado = int(input())
+
+  # cria um array com o nome e especie dos aniversariantes
   listaAniversariantes = []
   for i in range(numeroAnimais):
     date = dicionario["dataNascimento"][i].split("/")
     if int(date[1]) == mesSelecionado:
       listaAniversariantes.append([dicionario["nomeAnimal"][i], dicionario["especie"][i]])
+
   if not listaAniversariantes:
     print("Sem festa esse mes. :(")
   else:
     print("E os donos da festa do mes sao:")
+
+    # ordena a lista pelo valor da posicao 0
     listaAniversariantes.sort(key=lambda valor: valor[0])
     for i in listaAniversariantes:
       print(i[0], "-", i[1])
